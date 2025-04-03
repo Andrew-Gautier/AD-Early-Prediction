43e446ab0466271be8010db032cff940d7572f79
Andrew-Gautier <111550745+Andrew-Gautier@users.noreply.github.com>
2025-04-03 14:53:48 -0400

    Classifier work

---
+++ xgboost_classifier.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, roc_auc_score
+from xgboost import XGBClassifier
+from sklearn.impute import SimpleImputer
+
+
